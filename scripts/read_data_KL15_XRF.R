5ba0e181b01af27ac73aad8d93733606e8f0ff81
yokana65 <wagnerjoha.leipzig@gmail.com>
2024-06-07 11:07:52 +0200

    Read the KL15 XRF data and combine it with the Agemodel and interpolate the data for even time

--- scripts/read_data_KL15_XRF.R
+++ scripts/read_data_KL15_XRF.R
@@ -7,7 +7,7 @@ dir <- paste0(getwd(), "/data/Africa_NE_200/data/")
 # TODO: chain all commands with pipelines (readr)
 
 # read the sedimant data and the age model
-data_kl15_xrf <- read.table(paste0(dir,'data_KL15_XRF.txt'), header = TRUE, sep = "\t")
+data_kl15_xrf <- read.table(paste0(dir,'data_KL15_XRF.txt'), header = TRUE, sep = "\t") 
 data_kl15_agem <- read.table(paste0(dir,'data_KL15-2_smooth_spline_ages.txt'), header = TRUE, sep = "\t")
 data_kl15_qf <- read.table(paste0(dir,'data_KL15_qf.txt'), header = TRUE, sep = "\t")
 
@@ -15,31 +15,40 @@ data_kl15_qf <- read.table(paste0(dir,'data_KL15_qf.txt'), header = TRUE, sep = 
 data_kl15_xrf <- rename(data_kl15_xrf, depth = User_ID)
 data_kl15_agem <- rename(data_kl15_agem, age = best)
 
-# TODO: try to implement:
+# Perform an outer join to merge the age model with the XRF data by "depth"
+T <- full_join(data_kl15_agem, data_kl15_xrf, by = "depth") 
+col_names <- colnames(T[c(4,6:33)])
+missings_depth <- T[is.na(T[,6]), 1]
 
-# Perform an outer join
-T <- full_join(data_kl15_agem, data_kl15_xrf, by = "common_column") # replace "common_column" with the column you want to join by
+# remove QF marked measurements:
+# data_kl15 <- data_kl15[!data_kl15[,1] %in% data_kl15_qf[,1],]
+# TODO: ask markus: it does not remove anything; whats the purpose of this action?
 
-# Get the variable names
-data_kl15_string <- colnames(T)
-
-# Convert the data frame to a matrix
-data_kl15 <- as.matrix(T)
-
-# Remove rows where the first column's value is in the first column of data_kl15_qf
-data_kl15 <- data_kl15[!data_kl15[,1] %in% data_kl15_qf[,1],]
-
-# Remove rows where the sixth column's value is NA
+# Remove rows where the sixth column's value is NA => removes 49 NA's
 data_kl15 <- data_kl15[!is.na(data_kl15[,6]),]
 
-# Divide the fourth column by 1000
+# Divide the age column by 1000
 data_kl15[,4] <- data_kl15[,4] / 1000
 
-# Create a sequence from agemodelmin to agemodelmax with a step size of agemodelres
-data_kl15_age <- data.frame(matrix(ncol = 29, nrow = length(seq(agemodelmin, agemodelmax, by = agemodelres))))
-data_kl15_age[,1] <- seq(agemodelmin, agemodelmax, by = agemodelres)
+# Interpolating data to evenly spaced time axis.
+min <- min(data_kl15[,"age"])
+max <- max(data_kl15[,"age"])
+# TODO: check Resolution = (Max-Min)/Length => Resolution of around 260 years
+length <- nrow(data_kl15)
+resolution <- (max - min) / length
 
-# Interpolate data to evenly spaced time axis
-for (i in 2:29) {
-  data_kl15_age[,i] <- approx(data_kl15[,4], data_kl15[,i+5], xout = data_kl15_age[,1])$y
+# Create a sequence from agemodelmin to agemodelmax with a step size of agemodelres
+data_kl15_itpol <- data.frame(matrix(ncol = 29, nrow = length(seq(min, max, by = resolution))))
+data_kl15_itpol[,1] <- seq(min, max, by = resolution)
+
+
+# Interpolate data to evenly spaced time axis, that is fit the row values to the new time axis
+for (i in 2:28) {
+  data_kl15_itpol[,i] <- approx(data_kl15[,4], data_kl15[,i+5], xout = data_kl15_itpol[,1])$y
 }
+
+# get col names from data_kl15 and name data_kl15_itpol respectivaly
+colnames(data_kl15_itpol) <- col_names
+
+# define a colum "aggregate" that is the sum of all elements in a row except of the first column
+data_kl15_itpol$aggregate <- rowSums(data_kl15_itpol[2:28])
