3ed9cf2379e5e57a0ff5cef4abaf542b06b18e3b
yokana65 <wagnerjoha.leipzig@gmail.com>
2025-01-29 16:33:34 +0100

    Add more complex simulation setting

--- scripts/simulation_2.R
+++ scripts/simulation_2.R
@@ -27,3 +27,55 @@ build_setting_2comp_5parts_vs2 <- function(n_observations = 100,
 
   return(list("x_data" = x_data,"x_data_matrix" = x_data_matrix))
 }
+
+
+balanced_setting <- function(n_observations = 100,
+                            eigenvalues = c(0.7, 0.3, 0.0, 0.0),
+                            mean = c(0, 0.01, 0.02, -0.02, -0.01),
+                            n_counts = 500) {
+pc_1 <- c(0, sqrt(1/2), -sqrt(1/2), 0, 0)
+pc_2 <- c(-sqrt(1/2), 0, 0, sqrt(1/2), 0)
+lambda_1 <- eigenvalues[1]
+lambda_2 <- eigenvalues[2]
+
+clr_coords <- lapply(1:n_observations, function(i){
+  mean + rnorm(1, 0, sqrt(lambda_1))*pc_1 + rnorm(1, 0, sqrt(lambda_2))*pc_2
+})
+composition_list <- lapply(clr_coords, clrInv)
+x_data <- lapply(1:n_observations, function(i) {
+  probs <- composition_list[[i]]
+  rmultinom(1, n_counts, probs)[, 1]
+})
+x_data_matrix <- do.call(rbind, x_data)
+return(list("x_data" = x_data,"x_data_matrix" = x_data_matrix))
+}
+
+
+balanced_setting_vs2 <- function(n_observations = 100,
+                            eigenvalues = c(0.6, 0.3, 0.1, 0.1),
+                            mean = c(0, 0.01, 0.02, -0.02, -0.01),
+                            n_counts = 500) {
+pc_1 <- c(0, sqrt(1/2), -sqrt(1/2), 0, 0)
+pc_2 <- c(-sqrt(1/2), 0, 0, sqrt(1/2), 0)
+pc_3 <- c(sqrt(1/2), 0, -sqrt(1/2), 0, 0)
+pc_4 <- c(-sqrt(1/2), 0, 0, 0, sqrt(1/2))
+
+lambda_1 <- eigenvalues[1]
+lambda_2 <- eigenvalues[2]
+lambda_3 <- eigenvalues[3]
+lambda_4 <- eigenvalues[4]
+
+clr_coords <- lapply(1:n_observations, function(i){
+  mean + rnorm(1, 0, sqrt(lambda_1))*pc_1 +
+      rnorm(1, 0, sqrt(lambda_2)) * pc_2 +
+      rnorm(1, 0, sqrt(lambda_3)) * pc_3 +
+      rnorm(1, 0, sqrt(lambda_4)) * pc_4
+})
+composition_list <- lapply(clr_coords, clrInv)
+x_data <- lapply(1:n_observations, function(i) {
+  probs <- composition_list[[i]]
+  rmultinom(1, n_counts, probs)[, 1]
+})
+x_data_matrix <- do.call(rbind, x_data)
+return(list("x_data" = x_data,"x_data_matrix" = x_data_matrix))
+}
--- scripts/simulation_6.R
+++ scripts/simulation_6.R
@@ -49,3 +49,181 @@ build_setting_4comp_13parts_vs2 <- function(data,
 
   return(list("x_data" = x_data,"x_data_matrix" = x_data_matrix))
 }
+
+
+complex_setting <- function(data,
+  eigenvalues = c(0.43, 0.26, 0.12, 0.08),
+  mean = c(-1.21, -1.76, 1.70, -0.6,
+           1.04, -3.62, -1.26, 1.04, -0.83,
+           0.50, -4.0, -0.49, 1.51),
+  n_observations = nrow(data)) {
+
+  mean_data <- mean(data$aggregate)
+  sd_data <- sd(data$aggregate)
+
+  n_counts <- round(rnorm(n_observations, mean = mean_data, sd = sd_data))
+
+  pc_1 <- c(0.4, 0, 0.3, -0.3, 0, 0, -0.2, -0.2, 0.3, -0.1, 0.3, -0.3, -0.2)
+  pc_2 <- c(0.7, 0, -0.3, 0.2, 0, -0.2, 0, 0, 0, 0, -0.4, 0, 0)
+  pc_3 <- c(0.2, 0, -0.3, 0, -0.4, 0.6, -0.3, 0.2, 0, 0, 0, 0, 0)
+  pc_4 <- c(0.3, 0, 0, 0, 0, 0.2, 0, 0, -0.8, 0, 0.3, 0, 0)
+
+  normalize <- function(v) {
+    v / sqrt(sum(v^2))
+  }
+
+  pc_1_norm <- normalize(pc_1)
+  pc_2_norm <- normalize(pc_2)
+  pc_3_norm <- normalize(pc_3)
+  pc_4_norm <- normalize(pc_4)
+
+  lambda_1 <- eigenvalues[1]
+  lambda_2 <- eigenvalues[2]
+  lambda_3 <- eigenvalues[3]
+  lambda_4 <- eigenvalues[4]
+  
+  clr_coords <- lapply(1:n_observations, function(i){
+    mean + rnorm(1, 0, sqrt(lambda_1)) * pc_1_norm +
+      rnorm(1, 0, sqrt(lambda_2)) * pc_2_norm +
+      rnorm(1, 0, sqrt(lambda_3)) * pc_3_norm +
+      rnorm(1, 0, sqrt(lambda_4)) * pc_4_norm
+  })
+
+  composition_list <- lapply(clr_coords, clrInv)
+
+  x_data <- lapply(1:n_observations, function(i) {
+    probs <- composition_list[[i]]
+    rmultinom(1, n_counts[i] , probs)[, 1]
+  })
+  x_data_matrix <- do.call(rbind, x_data)
+
+  return(list("x_data" = x_data,"x_data_matrix" = x_data_matrix))
+}
+
+complex_setting_autoc <- function(data,
+                          eigenvalues = c(0.43, 0.26, 0.12, 0.08),
+                          mean = c(-1.21, -1.76, 1.70, -0.6,
+                                 1.04, -3.62, -1.26, 1.04, -0.83,
+                                 0.50, -4.0, -0.49, 1.51),
+                          n_observations = nrow(data),
+                          rho = 0.7) {
+
+  mean_data <- mean(data$aggregate)
+  sd_data <- sd(data$aggregate)
+  n_counts <- round(rnorm(n_observations, mean = mean_data, sd = sd_data))
+
+  pc_1 <- c(0.4, 0, 0.3, -0.3, 0, 0, -0.2, -0.2, 0.3, -0.1, 0.3, -0.3, -0.2)
+  pc_2 <- c(0.7, 0, -0.3, 0.2, 0, -0.2, 0, 0, 0, 0, -0.4, 0, 0)
+  pc_3 <- c(0.2, 0, -0.3, 0, -0.4, 0.6, -0.3, 0.2, 0, 0, 0, 0, 0)
+  pc_4 <- c(0.3, 0, 0, 0, 0, 0.2, 0, 0, -0.8, 0, 0.3, 0, 0)
+
+  normalize <- function(v) {
+    v / sqrt(sum(v^2))
+  }
+
+  pc_1_norm <- normalize(pc_1)
+  pc_2_norm <- normalize(pc_2)
+  pc_3_norm <- normalize(pc_3)
+  pc_4_norm <- normalize(pc_4)
+
+  generate_ar1 <- function(n, rho, sigma) {
+      eps <- rnorm(n, 0, sigma)
+      x <- numeric(n)
+      x[1] <- eps[1]
+      for(t in 2:n) {
+          x[t] <- rho * x[t-1] + eps[t]
+      }
+      return(x)
+  }
+  
+  # Generiere autokorrelierte Koeffizienten
+  coef1 <- generate_ar1(n_observations, rho, sqrt(eigenvalues[1]))
+  coef2 <- generate_ar1(n_observations, rho, sqrt(eigenvalues[2]))
+  coef3 <- generate_ar1(n_observations, rho, sqrt(eigenvalues[3]))
+  coef4 <- generate_ar1(n_observations, rho, sqrt(eigenvalues[4]))
+  
+  clr_coords <- lapply(1:n_observations, function(i){
+      mean + coef1[i]*pc_1_norm + coef2[i]*pc_2_norm + 
+            coef3[i]*pc_3_norm + coef4[i]*pc_4_norm
+  })
+  
+  composition_list <- lapply(clr_coords, clrInv)
+  
+  x_data <- lapply(1:n_observations, function(i) {
+      probs <- composition_list[[i]]
+      rmultinom(1, n_counts[i], probs)[, 1]
+  })
+  
+  x_data_matrix <- do.call(rbind, x_data)
+  
+  return(list("x_data" = x_data, "x_data_matrix" = x_data_matrix))
+}
+
+complex_setting_auto_sin <- function(data,
+                          eigenvalues = c(0.43, 0.26, 0.12, 0.08),
+                          mean = c(-1.21, -1.76, 1.70, -0.6,
+                                 1.04, -3.62, -1.26, 1.04, -0.83,
+                                 0.50, -4.0, -0.49, 1.51),
+                          n_observations = nrow(data),
+                          n_periods = 25) {
+  
+  mean_data <- mean(data$aggregate)
+  sd_data <- sd(data$aggregate)
+  n_counts <- round(rnorm(n_observations, mean = mean_data, sd = sd_data))
+  
+  # Bestehender Code für PCs...
+  pc_1 <- c(0.4, 0, 0.3, -0.3, 0, 0, -0.2, -0.2, 0.3, -0.1, 0.3, -0.3, -0.2)
+  pc_2 <- c(0.7, 0, -0.3, 0.2, 0, -0.2, 0, 0, 0, 0, -0.4, 0, 0)
+  pc_3 <- c(0.2, 0, -0.3, 0, -0.4, 0.6, -0.3, 0.2, 0, 0, 0, 0, 0)
+  pc_4 <- c(0.3, 0, 0, 0, 0, 0.2, 0, 0, -0.8, 0, 0.3, 0, 0)
+  
+  normalize <- function(v) {
+      v / sqrt(sum(v^2))
+  }
+  
+  pc_1_norm <- normalize(pc_1)
+  pc_2_norm <- normalize(pc_2)
+  pc_3_norm <- normalize(pc_3)
+  pc_4_norm <- normalize(pc_4)
+
+  generate_sin_coef <- function(n, periods, sigma) {
+      t <- seq(0, 2*pi*periods, length.out = n)
+      sin_wave <- sin(t)
+      return(sin_wave * sigma + rnorm(n, 0, sigma))
+  }
+
+  generate_cos_coef <- function(n, periods, sigma) {
+      t <- seq(0, 2*pi*periods, length.out = n)
+      sin_wave <- cos(t)
+      return(sin_wave * sigma + rnorm(n, 0, sigma))
+  }
+
+  coef1 <- generate_sin_coef(n_observations, n_periods, sqrt(eigenvalues[1]))
+  coef2 <- generate_cos_coef(n_observations, n_periods, sqrt(eigenvalues[2]))
+  coef3 <- generate_sin_coef(n_observations, n_periods, sqrt(eigenvalues[3]))
+  coef4 <- generate_cos_coef(n_observations, n_periods, sqrt(eigenvalues[4]))
+
+  clr_coords <- lapply(1:n_observations, function(i){
+    mean + coef1[i] * pc_1_norm + coef2[i]*pc_2_norm +
+      coef3[i] * pc_3_norm + coef4[i] * pc_4_norm
+  })
+
+  composition_list <- lapply(clr_coords, clrInv)
+
+  x_data <- lapply(1:n_observations, function(i) {
+      probs <- composition_list[[i]]
+      rmultinom(1, n_counts[i], probs)[, 1]
+  })
+
+  x_data_matrix <- do.call(rbind, x_data)
+
+  return(list("x_data" = x_data, "x_data_matrix" = x_data_matrix))
+}
+
+# plot(coef1, type = "l", col = "blue")
+# plot(coef1, type = "l", col = "blue", ylim = range(c(coef1, coef2, coef3, coef4)))
+# lines(coef2, col = "red")
+# lines(coef3, col = "green")
+# lines(coef4, col = "purple")
+# legend("topright", legend = c("PC1", "PC2", "PC3", "PC4"), 
+#        col = c("blue", "red", "green", "purple"), lty = 1)
